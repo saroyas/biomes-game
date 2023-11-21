5919ec4291b0aed1f8456dc9a4e15447f616705d
Mossy Mucker <110620858+mossymucker@users.noreply.github.com>
2023-11-21 06:31:40 +0000

    Update discord config

--- src/server/shared/config.ts
+++ src/server/shared/config.ts
@@ -192,6 +192,32 @@ const DEFAULT_CONFIG = deepFreeze({
   serviceDiscoveryTtlMs: 3_000,
   serviceDiscoveryServiceExpirySeconds: 24 * 60 * 60, // 24 hours
 
+  //
+  // Discord
+  //
+  discordHooksEnabled: false,
+  discordHookConfig: <DiscordHookConfig>{
+    badge: [],
+    deploy: ["discord-deploy-webhook-url"],
+    environment_group: [
+      "discord-social-webhook-url",
+      "discord-biomes-alpha-webhook",
+    ],
+    new_users: ["discord-new-users-webhook-url"],
+    photo: ["discord-camera-webhook-url", "discord-biomes-alpha-webhook"],
+    review: ["discord-review-webhook-url"],
+    social: ["discord-social-webhook-url", "discord-biomes-alpha-webhook"],
+    user_report: ["discord-user-report-webhook-url"],
+  },
+  discordGatingServerId: "692215223385587726",
+  discordEarlyAccessGeneralChannelId: "1176408479397838898",
+  discordPlaytesterRoleId: "706393450035019816",
+  discordPlaytesterRoleTimeThresholdMs: 1000 * 60 * 10, // 10 minutes.
+  discordRefreshMemberIntervalMs: 10 * 60 * 1000,
+  discordBotWelcomeMessage: `Beep! Boop! Welcome to The Collective!
+
+  I'm **B-42**, a friendly Biomes Robot here to transmit messages to you when players around the world visit your home, tag you in photos, or use any of the useful things you've built.`,
+
   // Asset server jitter interval
   assetServerJitterIntervalMs: 1000,
   assetServerPlayerMeshRecomputeIntervalMs: 1000 * 60 * 60 * 24, // 1 day
