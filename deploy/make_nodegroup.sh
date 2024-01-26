a6ce75b1102c4e59646f3a04f27ddb96c5f1b785
Mossy Mucker <110620858+mossymucker@users.noreply.github.com>
2024-01-26 17:37:54 +0000

    stuff

--- deploy/make_nodegroup.sh
+++ deploy/make_nodegroup.sh
@@ -1,8 +1,8 @@
 # How to set up your nodegroup if you are using AWS EKS.
 eksctl create nodegroup \
-  --cluster biomes-cluster2 \
+  --cluster biomes-cluster3 \
   --name biomes-nodegroup \
   --node-type m6i.2xlarge \
   --nodes-min 1 \
   --nodes-max 100 \
-  --nodes 5
+  --nodes 1
