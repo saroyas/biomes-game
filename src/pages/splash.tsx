bad7cb74d425d653649b612d76caa3e8225418b9
Mossy Mucker <110620858+mossymucker@users.noreply.github.com>
2024-03-17 01:15:48 +0000

    artists for ceasefire

--- src/pages/splash.tsx
+++ src/pages/splash.tsx
@@ -1,11 +1,50 @@
 import React, { useEffect, useState } from "react";
-
-import { WakeupMuckParticles } from "@/client/components/Particles";
 import { LoginRelatedController } from "@/client/components/static_site/LoginRelatedController";
 import { LoginRelatedControllerContext } from "@/client/components/static_site/LoginRelatedControllerContext";
 import { safeDetermineEmployeeUserId } from "@/server/shared/bootstrap/sync";
 
 import Head from "next/head";
+
+const FooterComponent = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        color: "#fff",
+        opacity: 0.7,
+        fontSize: "1rem",
+        marginTop: "100px",
+        marginBottom: "20px",
+      }}
+    >
+      {/* Wrap the image in an anchor (<a>) tag to make it clickable */}
+      <a
+        href="https://www.artists4ceasefire.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={"artistsforceasefire.png"}
+          alt="Artists for Ceasefire"
+          style={{
+            marginBottom: "10px",
+            maxWidth: "70px", // Adjust the size of the image to be smaller
+            height: "auto",
+            border: "none", // Makes the border transparent
+            display: "block", // Ensures the image is centered by using margin auto
+            marginLeft: "auto",
+            marginRight: "auto",
+            transition: "transform 0.3s ease", // Adds a smooth transition effect
+          }}
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.1)")} // Scales up the image on hover
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")} // Returns to original scale when not hovered
+        />
+      </a>
+      <p>Dedicated to the loving memory of our brother, Ismail.</p>
+      <p>Killed by Israeli forces at the age of 14.</p>
+    </div>
+  );
+};
 
 const DynamicBackgroundVideo = () => {
   const [videoSrc, setVideoSrc] = useState("");
@@ -90,7 +129,13 @@ export const SplashPage: React.FunctionComponent<{
   onLogin?: () => unknown;
 }> = ({ defaultUsernameOrId, onLogin }) => {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", // Ensure the container takes at least the full height of the viewport
+      }}
+    >
       <Head>
         <meta name="theme-color" content="#1C0D28" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -132,68 +177,79 @@ export const SplashPage: React.FunctionComponent<{
               {/* <WakeupMuckParticles /> */}
               {/* Use the showingModal state to conditionally render content */}
               {!loginRelatedControllerContext.showingModal && (
-                <div style={{ textAlign: "center" }}>
-                  <div
-                    style={{
-                      maxWidth: "600px",
-                      margin: "0 auto",
-                      paddingTop: "20vh",
-                    }}
-                  >
-                    <h1
-                      style={{
-                        fontSize: "2.5rem",
-                        fontWeight: "bold",
-                        marginBottom: "1rem",
-                      }}
-                    >
-                      Welcome to Oasis
-                    </h1>
-                    <p
-                      style={{
-                        fontSize: "1.25rem",
-                        marginBottom: "2rem",
-                        lineHeight: "1.5",
-                      }}
-                    >
-                      Build, forage, play minigames and more.
-                      <br />
-                      Explore this new community straight from your browser.
-                      <br />
-                      Currently in early beta testing. Things will break.
-                    </p>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        padding: "20px",
-                      }}
-                    >
-                      <EnterOasisButton
-                        onLoginClick={loginRelatedControllerContext.showLogin}
-                      />
-                    </div>
-                    <div
-                      style={{ fontSize: "1rem", color: "#fff", opacity: 0.7 }}
-                    >
-                      Brought to you by{" "}
-                      <a
-                        href="https://siliconsoul.xyz"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                <>
+                  <main style={{ flexGrow: 1 }}>
+                    <div style={{ textAlign: "center" }}>
+                      <div
+                        style={{
+                          maxWidth: "600px",
+                          margin: "0 auto",
+                          paddingTop: "20vh",
+                        }}
                       >
-                        Silicon Soul
-                      </a>
-                      .
+                        <h1
+                          style={{
+                            fontSize: "2.5rem",
+                            fontWeight: "bold",
+                            marginBottom: "1rem",
+                          }}
+                        >
+                          Welcome to Oasis
+                        </h1>
+                        <p
+                          style={{
+                            fontSize: "1.25rem",
+                            marginBottom: "2rem",
+                            lineHeight: "1.5",
+                          }}
+                        >
+                          Build, forage, play minigames and more.
+                          <br />
+                          Explore this new community straight from your browser.
+                          <br />
+                          Currently in early beta testing. Things will break.
+                        </p>
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            padding: "20px",
+                          }}
+                        >
+                          <EnterOasisButton
+                            onLoginClick={
+                              loginRelatedControllerContext.showLogin
+                            }
+                          />
+                        </div>
+                        <div
+                          style={{
+                            fontSize: "1rem",
+                            color: "#fff",
+                            opacity: 0.7,
+                          }}
+                        >
+                          Brought to you by{" "}
+                          <a
+                            href="https://siliconsoul.xyz"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Silicon Soul
+                          </a>
+                          .
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
+                  </main>
+                  <FooterComponent />
+                </>
               )}
             </>
           )}
         </LoginRelatedControllerContext.Consumer>
       </LoginRelatedController>
-    </>
+    </div>
   );
 };
 
